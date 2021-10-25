5e72e54f62dcd147e9ac1301a478824e00853026
luislodero <luislodero@gmail.com>
2021-10-25 14:07:59 -0700

    Add project files

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import Posts from '../components/Posts';
+import axios from 'axios';
+
+function Home() {
+  const [appState, setAppState] = useState({
+    loading: false,
+    posts: null,
+  });
+
+
+
+
+    useEffect( () => {
+
+        async function fetchData(){
+            setAppState({loading: true});
+            const apiURL = 'https://jsonplaceholder.typicode.com/posts';
+            const results = await axios.get(apiURL);
+            setAppState({loading: false , posts: results.data.slice(0,5)});
+        }
+        fetchData();
+  }, [setAppState]);
+
+  return (
+      <Posts isLoading={appState.loading} posts={appState.posts}/>
+  );
+
+}
+export default Home;
